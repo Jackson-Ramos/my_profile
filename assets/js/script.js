d1b4a3d88f89bba7ae7f60043c0c3b9d1674fe3a
Jackson Ramos <jackson.ramos@maisunifacisa.com.br>
2025-03-19 20:41:00 -0300

    reforação total

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,24 @@
-window.onscroll = function() {
+// Menu mobile
+document.addEventListener("DOMContentLoaded", function () {
+    const mobileBtn = document.querySelector('#mobile_btn');
+    const mobileMenu = document.querySelector('#mobile_menu');
+    const mobileIcon = mobileBtn.querySelector("i");
+
+
+    mobileBtn.addEventListener('click', function () {
+        mobileMenu.classList.toggle('active');
+        mobileIcon.classList.toggle('fa-x');
+    });
+
+});
+
+document.querySelector('#nav_logo').addEventListener('click', function (event) {
+    event.preventDefault();
+    location.reload();
+});
+
+// Scroll to top button
+window.onscroll = function () {
     let button = document.getElementById("topBtn");
     if (document.documentElement.scrollTop > 300) {
         button.style.display = "block";
@@ -10,3 +30,19 @@ window.onscroll = function() {
 function scrollToTop() {
     window.scrollTo({ top: 0, behavior: "smooth" });
 }
+
+// Intersection Observer animation
+const myObserver = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting == true) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => myObserver.observe(el));
+
+
